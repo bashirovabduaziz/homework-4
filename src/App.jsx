f1d4302089e7999ab2ebd831c8c766e7efa8a6e0
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2024-05-22 23:42:34 +0500

    hali chala

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./components/sections/Hero";
 import Section1 from "./components/sections/Section1";
 import Header from "./components/header/Header";
+import HorizontalAccordion from "./components/sections/HorizontalAccor";
 
 const App = () => {
   return (
     <div className="w-[1280px] mx-auto flex flex-col items-center justify-center">
       <Header />
       <Hero />
+      <HorizontalAccordion />
       <Section1 />
     </div>
   );
